197662d471d6bf91157b0a777c23ea2bcace3ee5
Airton <airtonbjunior@gmail.com>
2017-11-11 23:16:22 -0200

    Change parameters for new tests

--- variables.py
+++ variables.py
@@ -39,16 +39,16 @@ MAX_NEUTRAL_TWEETS   = 1400
 
 CROSSOVER = 0.9
 MUTATION = 0.1
-GENERATIONS = 51
-POPULATION  = 300
+GENERATIONS = 100
+POPULATION  = 100
 cicles_unchanged = 0
 generations_unchanged = 0
-max_unchanged_generations = 51
+max_unchanged_generations = 50
 max_unchanged_cicles = 9999999999
 
 generations_unchanged_reached_msg = False
 
-TOTAL_MODELS = 1
+TOTAL_MODELS = 3
 
 FILE_RESULTS = 'test_results-'+ str(datetime.now())[11:13] + str(datetime.now())[14:16] + str(datetime.now())[17:19] + '.txt'
 TRAIN_RESULTS = 'train-' + str(TOTAL_MODELS) + 'models_' + str(POPULATION) + 'p'+ str(GENERATIONS) +'g_' + str(datetime.now())[11:13] + str(datetime.now())[14:16] + str(datetime.now())[17:19] + '.txt'
