7f074431a5acfc07563a950cad36fbb53b853775
airtonbjunior <airtonbjunior@gmail.com>
2017-12-18 22:43:51 -0200

    dict defined, some comments

--- variables.py
+++ variables.py
@@ -6,6 +6,7 @@
 from nltk.corpus import stopwords
 from datetime import datetime
 
+# Paths
 SEMEVAL_TRAIN_FILE = 'datasets/train/twitter-train-cleansed-B.txt'
 SEMEVAL_TEST_FILE  = 'datasets/test/SemEval2014-task9-test-B-all-tweets_withSVMValues.txt'
 DICTIONARY_POSITIVE_WORDS = 'dictionaries/positive-words.txt'
@@ -51,6 +52,7 @@ slang_weight        = 1
 effect_weight       = 1
 semeval2015_weight  = 1
 
+# True: load the dictionary
 use_dic_liu          = True
 use_dic_sentiwordnet = True
 use_dic_affin        = True
@@ -59,6 +61,7 @@ use_dic_slang        = True
 use_dic_effect       = True
 use_dic_semeval2015  = True
 
+# Check if the dictionary was loaded
 dic_liu_loaded 			= False
 dic_sentiwordnet_loaded = False
 dic_affin_loaded		= False
@@ -69,12 +72,14 @@ dic_semeval2015_loaded	= False
 
 dic_loaded_total = 0
 
+# Balance the train tweets
 MAX_POSITIVES_TWEETS = 1400
 MAX_NEGATIVES_TWEETS = 1400
 MAX_NEUTRAL_TWEETS   = 1400
 
-CROSSOVER = 0.9
-MUTATION = 0.1
+# GP Parameters
+CROSSOVER   = 0.9
+MUTATION    = 0.1
 GENERATIONS = 51
 POPULATION  = 200
 cicles_unchanged = 0
@@ -86,54 +91,20 @@ generations_unchanged_reached_msg = False
 
 TOTAL_MODELS = 3
 
-FILE_RESULTS = 'test_results-' + str(datetime.now())[11:13] + str(datetime.now())[14:16] + str(datetime.now())[17:19] + '.txt'
+# Paths
+FILE_RESULTS  = 'test_results-' + str(datetime.now())[11:13] + str(datetime.now())[14:16] + str(datetime.now())[17:19] + '.txt'
 TRAIN_RESULTS = 'train-' + str(TOTAL_MODELS) + 'models_' + str(POPULATION) + 'p'+ str(GENERATIONS) +'g_' + str(datetime.now())[11:13] + str(datetime.now())[14:16] + str(datetime.now())[17:19] + '.txt'
 
-tweets_semeval       = []
-tweets_semeval_score = []
-svm_values_tweets    = []
-svm_is_neutral       = []
+tweets_semeval        = []
+tweets_semeval_score  = []
+svm_values_tweets     = []
+svm_is_neutral        = []
 svm_normalized_values = []
-tweet_semeval_index  = 0
+tweet_semeval_index   = 0
 
-dic_positive_words       = []
-dic_negative_words       = []
-
-dic_positive_words_sentiwordnet = []
-dic_negative_words_sentiwordnet = []
-dic_positive_value_sentiwordnet = []
-dic_negative_value_sentiwordnet = []
-
-dic_positive_words_semeval2015 = []
-dic_negative_words_semeval2015 = []
-dic_positive_value_semeval2015 = []
-dic_negative_value_semeval2015 = []
-
-dic_positive_words_slang = []
-dic_negative_words_slang = []
-dic_positive_value_slang = []
-dic_negative_value_slang = []
-
-dic_positive_words_affin = []
-dic_negative_words_affin = []
-dic_positive_value_affin = []
-dic_negative_value_affin = []
-
-dic_positive_words_effect = []
-dic_negative_words_effect = []
-dic_positive_value_effect = []
-dic_negative_value_effect = []
-
-dic_positive_words_vader = []
-dic_negative_words_vader = []
-dic_positive_value_vader = []
-dic_negative_value_vader = []
-
-#dic_positive_words_s140 = []
-#dic_negative_words_s140 = []
-#dic_positive_value_s140 = []
-#dic_negative_value_s140 = []
-
+# All are converted into sets because we don't need to keep the order
+dic_positive_words     = []
+dic_negative_words     = []
 dic_positive_hashtags  = []
 dic_negative_hashtags  = []
 dic_positive_emoticons = []
@@ -141,6 +112,26 @@ dic_negative_emoticons = []
 dic_negation_words     = []
 dic_booster_words      = []
 
+# Using python dictionary to improve the search performance
+dic_positive_semeval2015  = {}
+dic_negative_semeval2015  = {}
+
+dic_positive_slang 	      = {}
+dic_negative_slang        = {}
+
+dic_positive_affin        = {}
+dic_negative_affin        = {}
+
+dic_positive_sentiwordnet = {}
+dic_negative_sentiwordnet = {}
+
+dic_positive_effect 	  = {}
+dic_negative_effect 	  = {}
+
+dic_positive_vader        = {}
+dic_negative_vader        = {}
+
+# Counters
 positive_tweets = 0
 negative_tweets = 0
 neutral_tweets  = 0
@@ -149,6 +140,7 @@ fitness_positive = 0
 fitness_negative = 0
 fitness_neutral  = 0
 
+# Save the best values 
 best_fitness = 0
 best_fitness_history  = []
 best_fitness_per_generation_history = []
@@ -219,16 +211,18 @@ sms_2013_neutral  = 0
 all_messages_in_file_order = []
 all_polarities_in_file_order = []
 
-log_all_messages = False
 MAX_ANALYSIS_TWEETS = 10000
 
 false_neutral_log  = 0
 false_negative_log = 0
 false_positive_log = 0
 
-log_parcial_results = True
 log_all_metrics_each_cicle = False
-log_times = True
+log_all_messages           = False
+log_parcial_results        = True
+log_times           	   = True
+log_loads                  = True
+
 
 save_file_results = True
 
