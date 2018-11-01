5e2c4802b897977bba82b0cca31af10777b57160
airtonbjunior <airtonbjunior@gmail.com>
2018-11-01 17:30:49 -0200

    discard empty lines

--- testModel.py
+++ testModel.py
@@ -30,7 +30,7 @@ if __name__ == "__main__":
 
     with open(file_path, 'r') as inF:
         for line in inF:
-            if not line.startswith("[") and line not in ('\n', '\r\n') and not line.startswith("#"):
+            if not line.startswith("[") and line not in ('\n', '\r\n') and not line.startswith("#") and len(line.strip()) != 0:
                 functions_to_evaluate.append(str(line))
 
 
