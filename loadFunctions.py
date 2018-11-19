14b09b689ba24e7d5c8627006d915e9a8b26536c
airtonbjunior <airtonbjunior@gmail.com>
2018-11-19 16:19:09 -0200

    a lot of new load functions

--- loadFunctions.py
+++ loadFunctions.py
@@ -1,12 +1,87 @@
-# Airton Bordin Junior
-# airtonbjunior@gmail.com
-# Federal University of Goias (UFG)
-# Computer Science Master's Degree
+""" 
+<Author> Airton Bordin Junior
+<Email>  airtonbjunior@gmail.com
+
+Federal University of Goias (UFG)
+Computer Science Master's Degree
+
+
+loadFunctions.py
+	Functions that handle loads files/dictionaries/etc
+
+"""
 
 import time
 import variables
 
+
+def loadDictionaries():
+	"""Load the dictionaries words/polarities
+
+	The dic_words dictionary (defined in variables.py) will be populated using the follow format: 
+	
+		dic_words['dictionary_name']['positive']['word'] = polarity
+		dic_words['dictionary_name']['negative']['word'] = polarity
+
+		Example:
+			
+			Dictionary Sentiwordnet (word<tab>polarity)
+				able	0.125
+
+				dic_words['sentiwordnet']['positive']['able'] = 0.125
+	"""
+	start = time.time()
+	print("\n[loading dictionaries]")
+
+	for dic in variables.dictionaries:
+		if variables.use_dic[dic]:
+			variables.dic_loaded[dic] = True
+			variables.dic_words[dic]["positive"], variables.dic_words[dic]["negative"] = loadDictionaryValues(dic, variables.DIC_PATH[dic])
+
+	print("[all dictionaries (" + str(variables.dic_loaded_total) + ") loaded][" + str(format(time.time() - start, '.3g')) + " seconds]\n")
+
+
+def loadMessages(module, file_path, limit=0):
+	"""Load the messages
+
+		Args:
+			module    (str): train or test
+			file_path (str): path of the file
+			limit     (int): limit of messages loaded
+	"""
+	start = time.time()
+	print("\n[loading " + module + " tweets]")
+
+	msgs = {}
+	msgs_all = []
+
+	with open(file_path, "r") as f:
+		for line in f:
+			if limit == 0 or (limit != 0 and len(msgs_all) <= limit):
+				msgs['message'] = line.split('\t')[0].strip()
+				msgs['label']   = line.split('\t')[1].strip()
+				msgs_all.append(msgs)
+				msgs = {}
+
+	return msgs_all
+
+
+"""
+	Aux functions
+"""
 def loadDictionaryValues(dictionary_name, dictionary_path):
+	"""
+		Args:
+			dictionary_name (str):
+			dictionary_path (str):
+
+		Return
+			pos_words (dict):
+			neg_words (dict):
+
+
+		The dictionaries name are: "liu", "sentiwordnet", "afinn", "vader", "slang", "effect", "semeval2015", "nrc", "gi", "s140", "mpqa"
+	"""
 	startDic = time.time()
 	print("  [loading " + str(dictionary_name.strip().lower()) + "]")
 	variables.dic_loaded_total += 1
@@ -29,3 +104,10 @@ def loadDictionaryValues(dictionary_name, dictionary_path):
 		print("      [" + str(dictionary_name.strip().lower()) + " dictionary loaded][" + str(format(time.time() - startDic, '.3g')) + " seconds]\n")
 
 	return pos_words, neg_words
+
+
+def test():
+	variables.messages['train'] = loadMessages("train", "datasets/train/train_messages.txt", 10)
+	variables.messages['test']  = loadMessages("test", "datasets/test/test_messages.txt", 10)
+
+	print(str(variables.messages))
