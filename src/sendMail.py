9ff35495d1b4829c6bc25a2a13ce7faf772004d2
airtonbjunior <airtonbjunior@gmail.com>
2019-01-31 15:56:44 -0200

    pass the mail_content to send_mail function

--- src/sendMail.py
+++ src/sendMail.py
@@ -7,8 +7,9 @@
 
 import yagmail
 import platform
+import variables
 
-def send_mail(ith, total, pop, gen, content):
+def send_mail(ith, total, pop, gen, parameters):
 	FROM = 'sclassifier@gmail.com'
 	TO = 'sclassifier@gmail.com'
 	if(ith < total):
@@ -16,5 +17,10 @@ def send_mail(ith, total, pop, gen, content):
 	else:
 		SUBJECT = 'Last model created! [' + str(pop) + 'pop' + str(gen) + 'gen]' + '[' + platform.system() + ']'
 
+	mail_content = "Parameters: " + parameters + "\n\n" + str(variables.MODEL['bests'][len(variables.MODEL['bests']) - 1]) + "\n"
+	mail_content += "\n\nTotal tweets: " + str(variables.POSITIVE_MESSAGES + variables.NEGATIVE_MESSAGES + variables.NEUTRAL_MESSAGES) + " [" + str(variables.POSITIVE_MESSAGES) + " positives, " + str(variables.NEGATIVE_MESSAGES) + " negatives and " + str(variables.NEUTRAL_MESSAGES) + " neutrals]\n"
+	mail_content += "Fitness (F1 pos and neg): " + str(variables.BEST['fitness']) + " [" + str(variables.fitness_positive + variables.fitness_negative + variables.fitness_neutral) + " correct evaluations] ["+ str(variables.fitness_positive) + " positives, " + str(variables.fitness_negative) + " negatives and " + str(variables.fitness_neutral) + " neutrals]\n"
+	mail_content += "\nFitness evolution: " + str(variables.HISTORY['fitness']['best']) + "\n"
+
 	yag = yagmail.SMTP(FROM, 'sclassifier123')
-	yag.send(TO, SUBJECT, content)
+	yag.send(TO, SUBJECT, mail_content)
